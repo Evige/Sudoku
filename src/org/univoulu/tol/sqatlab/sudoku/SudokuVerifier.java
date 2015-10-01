52259a1e5084a2025f66a970be3c53ae98f9b006
Jussi Heikkilä <jussheik@hv040190.univ.yo.oulu.fi>
2015-10-01 13:47:23 +0300

    Correct solutions global rows
    Correct solutions global rows not including duplicates working.

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,9 +1,63 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SudokuVerifier {
+	
+	private String candidateSolution = "";
+	private List<Integer> expectedGlobalRow = new ArrayList();
+	private List<Integer> expectedGlobalColumn = new ArrayList();
 
 	public int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
-		return 0;
+		this.candidateSolution = candidateSolution;
+		int result = 0;
+		
+		List<Integer> solutionNumbers = new ArrayList();
+		
+		for (int x = 0; x < 81; x++) {
+			int number = Integer.parseInt(String.valueOf(candidateSolution.charAt(x)));
+			solutionNumbers.add(number);
+		}
+		
+		result = checkDuplicatesInGlobalRows(solutionNumbers);
+		
+		return result;
+	}
+	
+	private int checkDuplicatesInGlobalRows(List<Integer> solutionNumbers) {
+		int result = 0;
+		
+		int looper = 0;
+		int solutionDigit = 0;
+		while (looper < 9) {
+			int y = 1;
+			expectedGlobalRow = new ArrayList();
+			List<Integer> checkedSolutionRow = new ArrayList();
+			for (y = 1; y <= 9; y++) {
+				expectedGlobalRow.add(y);
+				checkedSolutionRow.add(solutionNumbers.get(solutionDigit));
+				solutionDigit = solutionDigit + 1;
+			}
+			Collections.sort(checkedSolutionRow);
+			
+			if (!expectedGlobalRow.equals(checkedSolutionRow)) {
+				result = -3;
+				break;
+			}
+			looper = looper + 1;
+		}
+		
+		return result;
+	}
+	
+	public void setCandidateSolution(String candidateSolution) {
+		this.candidateSolution = candidateSolution;
+	}
+	
+	public String getCandidateSolution() {
+		return candidateSolution;
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,13 +2,76 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
+	
+	private SudokuVerifier verifier;
+	private List<Integer> expectedResult = new ArrayList();
+	private String correctSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	private String incorrectSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	
+	@Before
+	public void setup() {
+		verifier = new SudokuVerifier();
+		
+		int looper = 0;
+		while (looper < 9) {
+			int y = 1;
+			for (y = 1; y <= 9; y++) {
+				expectedResult.add(y);	
+			}
+			looper = looper + 1;
+		}
+		Collections.sort(expectedResult);
+	}
+	
+	@Test
+	public void testFirstRowWontContainDuplicatesCorrectSolution() {
+		List<Integer> firstRow = new ArrayList();
+		List<Integer> expectedFirstRow = new ArrayList();
+		
+		for (int x = 0; x < 9; x++) {
+			int number = Integer.parseInt(String.valueOf(correctSolution.charAt(x)));
+			firstRow.add(number);
+		}
+		
+		for (int y = 1; y <= 9; y++) {
+			expectedFirstRow.add(y);	
+		}
+		
+		Collections.sort(firstRow);
+		
+		assertEquals(expectedFirstRow, firstRow);
+	}
+	
+	@Test
+	public void testGlobalRowsWontContainDuplicates() {
+		int result = verifier.verify(correctSolution);
+		
+		assertEquals(0, result);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSolution81LongWithCorrectSolution() {
+		verifier.verify(correctSolution);
+		String solution = verifier.getCandidateSolution();
+		
+		assertEquals(81, solution.length());
+	}
+	
+	@Test
+	public void testSolution81LongWithIncorrectSolution() {
+		verifier.verify(incorrectSolution);
+		String solution = verifier.getCandidateSolution();
+		
+		assertEquals(81, solution.length());
 	}
 
 }
